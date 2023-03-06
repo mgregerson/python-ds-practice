0bcc6d49d687dff3a3d4a8dd1ae0dd5ffc842613
mgregerson <mgregerson4321@gmail.com>
2023-03-06 18:07:38 -0500

    initial commit

---
+++ 03_last_element.py
@@ -0,0 +1,15 @@
+def last_element(lst):
+    """Return last item in list (None if list is empty.
+
+        >>> nums = [1, 2, 3]
+        >>> last_element(nums)
+        3
+        
+    Make sure original list has not been mutated:
+
+        >>> nums == [1, 2, 3]
+        True
+
+        >>> last_element([]) is None
+        True
+    """
